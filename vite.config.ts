83c8511d100c9b0525cd231a892f37bf069566a0
Dinh-Van Colomban <van.colomban@gmail.com>
2023-04-03 21:49:42 +0200

    fix(sw): fix pwa image in dist

--- vite.config.ts
+++ vite.config.ts
@@ -19,17 +19,17 @@ export default defineConfig({
         theme_color: '#282c34',
         icons: [
           {
-            src: 'src/assets/pwa/icons/192.png',
+            src: 'assets/pwa-icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'src/assets/pwa/icons/512.png',
+            src: 'assets/pwa-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'src/assets/pwa/icons/512.png',
+            src: 'assets/pwa-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
