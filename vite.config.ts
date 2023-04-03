190048599d8983fe3d755c7ae8d4f0029bce1c9d
Dinh-Van Colomban <van.colomban@gmail.com>
2023-04-03 19:27:24 +0200

    feat(sw): adds service worker

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,48 @@ import { fileURLToPath, URL } from 'url';
 
 import suidPlugin from '@suid/vite-plugin';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [suidPlugin(), solidPlugin()],
+  plugins: [
+    suidPlugin(),
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'logo.svg', 'pwa/icon.png'],
+      manifest: {
+        name: 'Dinh-Van Colomban',
+        short_name: 'Dinh-Van',
+        description: "Web app for Dinh-Van's projects",
+        theme_color: '#282c34',
+        icons: [
+          {
+            src: 'src/assets/pwa/icon/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'src/assets/pwa/icon/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'src/assets/pwa/icon/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: !!process.env.SERVICE_WOKER,
+      },
+      workbox: {
+        sourcemap: !!process.env.SERVICE_WOKER,
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
