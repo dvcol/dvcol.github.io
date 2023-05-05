36a71c84e486f94cb314afe7da1b7234ae2da5f6
Dinh-Van Colomban <van.colomban@gmail.com>
2023-05-06 01:19:27 +0200

    fix(navbar): switch to eventlistner

--- src/components/common/navbar/navbar-button.tsx
+++ src/components/common/navbar/navbar-button.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@suid/material';
 
-import { createMemo } from 'solid-js';
+import { createMemo, createSignal, onCleanup, onMount } from 'solid-js';
 
 import styles from './navbar-buttion.module.scss';
 
@@ -8,29 +8,57 @@ import type { Component } from 'solid-js';
 
 import { NavbarButtonId, useNavbar, useRouteData } from '~/services';
 
+const boundary = { collapse: 200, expand: 100 };
+
 export const NavbarButton: Component = () => {
   const { isScrolled, isOpen, toggle } = useNavbar();
   const { active } = useRouteData();
   const accent = createMemo(() => active()?.accentColor || active()?.color);
 
+  const [hover, setHover] = createSignal<keyof typeof boundary | false>('collapse');
+
+  const onMouseMove = ({ clientX, clientY }: MouseEvent) => {
+    const offsetX = window.innerWidth - clientX;
+    const offsetY = clientY;
+
+    if (hover() !== 'collapse' && (offsetX > boundary.collapse || offsetY > boundary.collapse)) {
+      setHover('collapse');
+    } else if (
+      hover() !== 'expand' &&
+      offsetX < boundary.collapse &&
+      offsetY < boundary.collapse &&
+      (offsetX > boundary.expand || offsetY > boundary.expand)
+    ) {
+      setHover('expand');
+    } else if (hover() && offsetX <= boundary.expand && offsetY <= boundary.expand) {
+      setHover(false);
+    }
+  };
+
+  onMount(() => window.addEventListener('mousemove', onMouseMove));
+  onCleanup(() => window.removeEventListener('mousemove', onMouseMove));
+
+  const collapsed = createMemo<boolean>(() => !!isScrolled() && hover() === 'collapse');
+  const expand = createMemo<boolean>(() => !!isScrolled() && hover() === 'expand');
+
   return (
-    <Box class={styles.menu_button__container} classList={{ [styles.menu_button__collapsed]: !!isScrolled() }}>
-      <Box class={styles.menu_button__trigger}>
-        <Box
-          component="button"
-          id={NavbarButtonId}
-          class={styles.menu_button}
-          classList={{ [styles.menu_button__open]: isOpen() }}
-          sx={{
-            '& span': { color: accent() },
-            '&::before': { color: accent() },
-            '&::after': { color: accent() },
-          }}
-          onClick={() => toggle()}
-        >
-          <span>Menu</span>
-        </Box>
-      </Box>
+    <Box
+      component="button"
+      id={NavbarButtonId}
+      class={styles.menu_button}
+      classList={{
+        [styles.menu_button__open]: isOpen(),
+        [styles.menu_button__collapsed]: collapsed(),
+        [styles.menu_button__expand]: expand(),
+      }}
+      sx={{
+        '& span': { color: accent() },
+        '&::before': { color: accent() },
+        '&::after': { color: accent() },
+      }}
+      onClick={() => toggle()}
+    >
+      <span>Menu</span>
     </Box>
   );
 };
