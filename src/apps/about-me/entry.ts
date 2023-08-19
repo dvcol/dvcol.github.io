74760af6bd1add26d9855a2b3409dbbf2aee0d76
Dinh-Van Colomban <van.colomban@gmail.com>
2023-08-19 17:38:07 +0200

    chore: adds about-me dev script

--- src/apps/about-me/entry.ts
+++ src/apps/about-me/entry.ts
@@ -14,7 +14,8 @@ declare module 'solid-js' {
 
 const defineAboutMeComponents = async () => {
   if (customElements.get('wc-about-me')) return;
-  const aboutMe: AboutMe = await import(/* @vite-ignore  */ `${AppLink.pages}/${baseUrl}/entry/index.js`);
+  const domain = import.meta.env.VITE_ABOUT_ME ?? AppLink.pages;
+  const aboutMe: AboutMe = await import(/* @vite-ignore  */ `${domain}/${baseUrl}/entry/index.js`);
   aboutMe.defineComponent();
 };
 
