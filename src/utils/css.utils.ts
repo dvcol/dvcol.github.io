13bbb25779886757c4417b5a5c6104615d493741
Dinh-Van Colomban <van.colomban@gmail.com>
2023-09-03 19:50:12 +0200

    fix(gradient): fit content width

--- src/utils/css.utils.ts
+++ src/utils/css.utils.ts
@@ -2,10 +2,11 @@ export type GradientFromTo = { from: string; to: string };
 export type GradientOptions = { start?: number; end?: number; direction?: number };
 
 export const gradientText = ({ from, to }: GradientFromTo, options: GradientOptions = {}) => {
-  const { start, end, direction } = { direction: '120deg', start: 0, end: 50, ...options };
+  const { start, end, direction } = { direction: '145deg', start: 0, end: 80, ...options };
   return {
     background: `linear-gradient(${direction}, ${from} ${start}%, ${to} ${end}%);`,
     WebkitBackgroundClip: 'text',
     WebkitTextFillColor: 'transparent',
+    width: 'fit-content',
   };
 };
