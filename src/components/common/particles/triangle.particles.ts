9e1fe0b87d9a73316370559f66dc9ba14742b260
Dinh-Van Colomban <van.colomban@gmail.com>
2024-04-08 17:04:49 +0200

    fix(particles): slow down and color shift particles

--- src/components/common/particles/triangle.particles.ts
+++ src/components/common/particles/triangle.particles.ts
@@ -5,14 +5,14 @@ export const TriangleParticles: ParticlesProps['options'] = {
   delay: 0,
   particles: {
     number: {
-      value: 100,
+      value: 5,
       density: {
         enable: true,
-        area: 800,
+        area: 100,
       },
     },
     color: {
-      value: '#ff0000',
+      value: '#fff',
       animation: {
         enable: true,
         speed: 10,
@@ -44,14 +44,14 @@ export const TriangleParticles: ParticlesProps['options'] = {
     },
     links: {
       enable: true,
-      distance: 100,
-      color: '#ffffff',
-      opacity: 0.4,
+      distance: 200,
+      color: 'rgba(255,255,255,0.2)',
+      opacity: 0.2,
       width: 1,
     },
     move: {
       enable: true,
-      speed: 2,
+      speed: 0.8,
       direction: 'none',
       random: false,
       straight: false,
