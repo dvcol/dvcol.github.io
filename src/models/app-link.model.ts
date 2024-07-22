db88ee063e082d827a4f6c9dbd2ccd160e82fb7c
Dinh-Van Colomban <van.colomban@gmail.com>
2024-07-22 14:35:16 +0200

    fix(links): update linkedin external link

--- src/models/app-link.model.ts
+++ src/models/app-link.model.ts
@@ -1,7 +1,9 @@
 export enum AppLink {
   pages = 'https://dvcol.github.io',
   github = 'https://github.com/dvcol',
-  linkedIn = 'https://linkedin.com/in/dinh-van-colomban-76b513a4',
+  linkedIn = 'https://linkedin.com/in/dinh-van-colomban',
   synologyWebStore = 'https://chrome.google.com/webstore/detail/download-station-client-f/ebbdkledlkjpgbbmmopgbnnjmklnkcef',
   synologyGithub = 'https://github.com/dvcol/synology-download',
+  traktWebStore = 'https://chromewebstore.google.com/detail/side-trakt-companion-app/pdodapikbijcfickiofjkjgkkmlcnbba',
+  traktGithub = 'https://github.com/dvcol/trakt-extension',
 }
